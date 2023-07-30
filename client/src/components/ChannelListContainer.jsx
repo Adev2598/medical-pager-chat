9da44026274bc840bc6cbb65f1ab1bd7c3b94d05
Adev2598 <ammar.react.dev@outlook.com>
2023-07-30 15:41:01 +0400

    stage 2

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -4,6 +4,7 @@ import Cookies from 'universal-cookie';
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview} from './';
 import HospitalIcon from '../assets/hospital.png';
+import LogoutIcon from '../assets/logout.png';
 
 const SideBar = () => (
   <div className='channel-list__sidebar'>
@@ -12,13 +13,46 @@ const SideBar = () => (
             <img src={HospitalIcon} alt='Hospital' width='30'/>
         </div>
     </div>
+    <div className='channel-list__sidebar__icon2'>
+        <div className='icon1__inner'>
+            <img src={LogoutIcon} alt='Logout' width='30'/>
+        </div>
+    </div>
+  </div>
+);
+
+const CompanyHeader = () => (
+  <div className='channel-list__header'>
+    <p className='channel-list__header__text'>Medical Pager</p>
   </div>
 )
 
 const ChannelListContainer = () => {
   return (
-    <div>ChannelListContainer</div>
-  )
+    <>
+      <SideBar />
+      <div className='channel-list__list__wrapper'>
+        <CompanyHeader />
+        <ChannelSearch />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => (
+            <TeamChannelList
+              {...listProps}
+              type='team'
+            />
+          )}
+          Preview={(previewProps) => (
+            <TeamChannelPreview 
+              {...previewProps}
+              type='team'
+            />
+          )}    
+        />
+      </div>
+    </>
+  );
 }
 
-export default ChannelListContainer
+export default ChannelListContainer;
---
+++ client/src/components/TeamChannelPreview.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Avatar, useChatContext } from 'stream-chat-react';
+
+const TeamChannelPreview = ({ channel, type }) => {
+
+  const { channel: activeChannel, client } = useChatContext();
+
+  const ChannelPreview = () => (
+    <p className='channel-preview__item'>
+        # {channel.data?.name || channel?.data?.id}
+    </p>
+  );
+
+  const DirectPreview = () => {
+    const members = Object.values(channel.state.members).filter(({ user }) => user.id !== client.userID);
+  }
+
+  return (
+    <div>TeamChannelPreview</div>
+  )
+}
+
+export default TeamChannelPreview
