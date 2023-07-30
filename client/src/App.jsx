9fa8e859d3b0afff64b45587bced80ffc5d9e39a
Adev2598 <ammar.react.dev@outlook.com>
2023-07-30 10:39:38 +0400

    stage 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
+import './App.css';
+
 import { ChannelListContainer, ChannelContainer } from './components';
 
 const apiKey = 'y6saru9jduns';
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,4 +1,19 @@
 import React from 'react'
+import { ChannelList, useChatContext } from 'stream-chat-react'
+import Cookies from 'universal-cookie';
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview} from './';
+import HospitalIcon from '../assets/hospital.png';
+
+const SideBar = () => (
+  <div className='channel-list__sidebar'>
+    <div className='channel-list__sidebar__icon1'>
+        <div className='icon1__inner'>
+            <img src={HospitalIcon} alt='Hospital' width='30'/>
+        </div>
+    </div>
+  </div>
+)
 
 const ChannelListContainer = () => {
   return (
