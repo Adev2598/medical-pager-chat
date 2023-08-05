aa0058a2833f26330f6104d5130753c3a1c61136
Ammar Al Harthy <138360246+Adev2598@users.noreply.github.com>
2023-08-05 20:45:19 +0400

    structure done

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -13,11 +13,32 @@ const TeamChannelPreview = ({ channel, type }) => {
 
   const DirectPreview = () => {
     const members = Object.values(channel.state.members).filter(({ user }) => user.id !== client.userID);
+    return(
+      <div className='channel-preview__item single'>
+        <Avatar
+          image={members[0]?.user?.image}
+          name={members[0]?.user?.fullname}
+          size={24}
+        />
+        <p>name={members[0]?.user?.fullname}</p>
+      </div>
+
+    )
   }
 
   return (
-    <div>TeamChannelPreview</div>
-  )
+    <div className={
+      channel?.id === activeChannel?.id
+      ? 'channel-preview__wrapper__selected'
+      : 'channel-preview__wrapper'
+    }
+      onClick={() => {
+        console.log(channel);
+      }}
+    > 
+      { type === 'team' ? <ChannelPreview/> : <DirectPreview/>}
+    </div>
+  );
 }
 
 export default TeamChannelPreview
